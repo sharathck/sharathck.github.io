747406a69a0f298ccbbef4ab6cef9399dab966e1
Sharath Chandra <12277497+sharathck@users.noreply.github.com>
2019-12-30 16:46:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ var loadtodolist = function () {
           querySnapshot.forEach((doc) => {
             console.log(querySnapshot);
             console.log('local   - ' + doc.data().title + doc.data().dueDate);
-            var listItem = createNewTaskElement(doc.data().title, doc.id);
+            var listItem = createNewTaskElement('* ' + doc.data().title, doc.id);
             //Append listItem to incompleteTasksHolder
             incompleteTasksHolder.appendChild(listItem);
             bindTaskEvents(listItem, taskCompleted);
