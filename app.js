fc0a22af988ccb7c753637c6afaec848eae14784
Sharath Chandra <12277497+sharathck@users.noreply.github.com>
2020-01-02 09:29:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ var loadtodolist = function () {
 
       firebase.firestore().disableNetwork();
       console.log('from cache  -  ' + useremail + ';');
-      db.collection("tasks").where("uemail", "==", useremail).where("dueDate", "<=", currDate).where("status", "==", false).orderBy("dueDate", "desc").get()
+      db.collection("tasks").where("uemail", "==", useremail).where("dueDate", "<=", currDate).where("status", "==", false).orderBy("dueDate", "desc").get(Source.CACHE)
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
             console.log(querySnapshot);
@@ -145,7 +145,7 @@ var loadtodolist = function () {
           });
           console.log('from server  -  ' + useremail + ';');
           firebase.firestore().enableNetwork();
-          db.collection("tasks").where("uemail", "==", useremail).where("dueDate", "<=", currDate).where("status", "==", false).orderBy("dueDate", "desc").get()
+          db.collection("tasks").where("uemail", "==", useremail).where("dueDate", "<=", currDate).where("status", "==", false).orderBy("dueDate", "desc").get(Source.SERVER)
             .then((squerySnapshot) => {
               squerySnapshot.forEach((sdoc) => {
                 //   console.log('server   - ' + sdoc.data().title);
